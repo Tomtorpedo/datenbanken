3ef151bb76fae608d09f301c12bef9b2011d717e
tomtorpedo <vervoort.tom@proton.me>
2025-04-29 10:52:29 +0200

    modified:   mlb/aufgabe12.sql

--- mlb/aufgabe12.sql
+++ mlb/aufgabe12.sql
@@ -1,17 +1,20 @@
-SELECT 
-    first_name,last_name,ROUND((weight*0.4536),2) AS "weight_kg"
-FROM players
-WHERE
-    weight IS NOT Null
-    AND 
+SELECT first_name, last_name, weight_kg
+FROM (
+    SELECT first_name, last_name, ROUND((weight*0.453592), 2) AS weight_kg
+    FROM players
+    WHERE weight IS NOT NULL
+    ORDER BY weight ASC
+    LIMIT 5
+) AS lightest
+
 UNION
-SELECT 
-    first_name,last_name,ROUND((weight*0.4536),2) AS "weight_kg"
-FROM players
-WHERE
-    weight IS NOT Null
-ORDER BY
-    weight,last_name
-LIMIT
-    5
-;
+
+SELECT first_name, last_name, weight_kg
+FROM (
+    SELECT first_name, last_name, ROUND((weight*0.453592), 2) AS weight_kg
+    FROM players
+    ORDER BY weight DESC
+    LIMIT 5
+) AS heaviest
+
+ORDER BY weight_kg , last_name;
